eb8ec9cb8df1142c34c87041fe470dbb28b89cbe
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-07-16 10:05:01 +0800

    auto submit

--- exhibit/static/exhibit/js/shop_detail.js
+++ exhibit/static/exhibit/js/shop_detail.js
@@ -104,6 +104,7 @@ var initTimepicker = function() {
 var initLocationAction = function() {
   wxConfig(['getLocation', 'openLocation']);
   wx.ready(function() {
+    console.log('wx is ready');
     wx.getLocation({
         type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
         success: function (res) {
