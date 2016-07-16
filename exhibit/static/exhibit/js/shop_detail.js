12e5fee8efaeb0e557865e312daa209182488fcc
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-07-16 11:19:18 +0800

    auto submit

--- exhibit/static/exhibit/js/shop_detail.js
+++ exhibit/static/exhibit/js/shop_detail.js
@@ -112,11 +112,10 @@ var initLocationAction = function() {
             var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
             var speed = res.speed; // 速度，以米/每秒计
             var accuracy = res.accuracy; // 位置精度
-            alert('当前位置:' + latitude + ' ' + longitude);
         }
     });
     getAddressLocation(address, function(loc) {
-      alert('lat: ' + loc[0] + ' long: ' + loc[1]);
+      alert(loc);
       wx.openLocation({
         latitude: loc[0], // 纬度，浮点数，范围为90 ~ -90
         longitude: loc[1], // 经度，浮点数，范围为180 ~ -180。
