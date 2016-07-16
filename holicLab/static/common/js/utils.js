81d1eb8540c2e235c575bc4a5aaae9fd229a4a21
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-07-17 00:12:37 +0800

    auto submit

--- holicLab/static/common/js/utils.js
+++ holicLab/static/common/js/utils.js
@@ -50,7 +50,7 @@ var wxConfig = function(jsApiList) {
     var signature = msg['signature'];
     var timestamp = msg['timestamp'];
     var nonceStr = msg['noncestr'];
-    console.log(signature);
+    alert(msg.join('; ') + ' 开始初始化jsapi');
     wx.config({
       debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
       appId: appId, // 必填，公众号的唯一标识
