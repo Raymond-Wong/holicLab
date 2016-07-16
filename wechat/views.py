b5fb5d8eac7e491db1eef479478de82bd70b41c2
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-07-16 09:01:26 +0800

    auto submit

--- wechat/views.py
+++ wechat/views.py
@@ -37,6 +37,21 @@ def entrance(request):
       return HttpResponse('forbiden from browswer')
   raise Http404
 
+def config(request):
+  return HttpResponse(json.dumps(get_ticket(request.GET.get('type', 1))))
+
+# 获取某种类型的ticket，1为access token，2为jsapi
+def get_ticket(ticket_type):
+  records = Ticket.objects.filter(ticket_type=ticket_type)
+  toRefresh = True
+  if len(records) > 0:
+    record = records.orer_by('-start_time')[0]
+    if (timezone.now - record.end_time).seconds < 7200:
+      toRefresh = False
+  if toRefresh:
+    record = update_token() if ticket_type == 1 else update_jsapi()
+  return record
+
 # 验证信息是否从微信发送过来
 def verify(token, timestamp, nonce, signature):
   tmpList = [token, timestamp, nonce]
@@ -69,8 +84,37 @@ def update_token():
   if len(token_record) > 0:
     token_record = token_record[0]
   else:
-    token_record = access_token()
+    token_record = Ticket()
   token_record.content = token
   token_record.end_time = endtime
+  token_record.save()
+  return token_record
+
+def update_jsapi():
+  params = {
+    'access_token': get_ticket(1).content,
+    'type': jsapi,
+  }
+  host = 'api.weixin.qq.com'
+  path = '/cgi-bin/ticket/getticket'
+  method = 'GET'
+ 
+  res = send_request(host, path, method, params=params)
+  if not res[0]:
+    return False
+  if res[1].get('errcode'):
+    return False
+  token = res[1].get('ticket')
+  starttime = timezone.now()
+  expires_in = timedelta(seconds=int(res[1].get('expires_in')))
+  endtime = starttime + expires_in
+  token_record = Ticket.objects.filder(ticket_type=2).order_by('-start_time')
+  if len(token_record) > 0:
+    token_record = token_record[0]
+  else:
+    token_record = Ticket()
+  token_record.content = token
+  token_record.end_time = endtime
+  token_record.ticket_type = 2
   token_record.save()
   return token_record
