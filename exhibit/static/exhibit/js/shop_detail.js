a9d47b0ef3020b197163bd9689923aa745a80c21
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-07-16 11:16:29 +0800

    auto submit

--- exhibit/static/exhibit/js/shop_detail.js
+++ exhibit/static/exhibit/js/shop_detail.js
@@ -105,8 +105,8 @@ var initLocationAction = function() {
   wxConfig(['getLocation', 'openLocation']);
   wx.ready(function() {
     var address = $('.locationBox').text();
-    alert(address);
     getAddressLocation(address, function(loc) {
+      alert('lat: ', loc[0], ' long: ', loc[1]);
       wx.openLocation({
         latitude: loc[0], // 纬度，浮点数，范围为90 ~ -90
         longitude: loc[1], // 经度，浮点数，范围为180 ~ -180。
