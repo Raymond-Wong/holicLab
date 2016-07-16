eba14a2f9be2e4074190895d105294919c1c3a40
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-07-17 00:13:39 +0800

    auto submit

--- static/common/js/utils.js
+++ static/common/js/utils.js
@@ -45,7 +45,8 @@ function getUrlParam(name) {
 
 var wxConfig = function(jsApiList) {
   var appId = 'wx466a0c7c6871bc8e';
-  var url = window.location.href.split('#')[0]
+  var url = window.location.href.split('#')[0];
+  alert('wxConfig');
   post('/wechat/config', {'url' : url}, function(msg) {
     var signature = msg['signature'];
     var timestamp = msg['timestamp'];
