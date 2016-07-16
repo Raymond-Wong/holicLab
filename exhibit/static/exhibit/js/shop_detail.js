2483a5e556ae796bf7cc9b8e5d0a9f0ca17b84bc
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-07-16 12:07:04 +0800

    auto submit

--- exhibit/static/exhibit/js/shop_detail.js
+++ exhibit/static/exhibit/js/shop_detail.js
@@ -105,20 +105,22 @@ var initLocationAction = function() {
   wxConfig(['getLocation', 'openLocation']);
   wx.ready(function() {
     var address = $('.locationBox').text();
+    var lat = null;
+    var lon = null;
     wx.getLocation({
         type: 'gcj02', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
         success: function (res) {
-            var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-            var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+            lat = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+            lon = res.longitude; // 经度，浮点数，范围为180 ~ -180。
             var speed = res.speed; // 速度，以米/每秒计
             var accuracy = res.accuracy; // 位置精度
         }
     });
     getAddressLocation(address, function(result) {
-      alert(result);
+      alert(result.latitude);
       wx.openLocation({
-        latitude: 22.562163, // 纬度，浮点数，范围为90 ~ -90
-        longitude: 113.873918, // 经度，浮点数，范围为180 ~ -180。
+        latitude: lat, // 纬度，浮点数，范围为90 ~ -90
+        longitude: lon, // 经度，浮点数，范围为180 ~ -180。
         name: '全能工作室', // 位置名
         address: '深圳市宝安中心新安六路与宝源南路交界处众里创业社区首层', // 地址详情说明
         scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
