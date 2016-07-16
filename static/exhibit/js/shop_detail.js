5c24734dd11b0b60da1e8e93bfd29c54deeecb45
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-07-16 11:18:06 +0800

    auto submit

--- static/exhibit/js/shop_detail.js
+++ static/exhibit/js/shop_detail.js
@@ -105,6 +105,16 @@ var initLocationAction = function() {
   wxConfig(['getLocation', 'openLocation']);
   wx.ready(function() {
     var address = $('.locationBox').text();
+    wx.getLocation({
+        type: 'gcj02', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+        success: function (res) {
+            var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+            var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+            var speed = res.speed; // 速度，以米/每秒计
+            var accuracy = res.accuracy; // 位置精度
+            alert('当前位置:' + latitude + ' ' + longitude);
+        }
+    });
     getAddressLocation(address, function(loc) {
       alert('lat: ' + loc[0] + ' long: ' + loc[1]);
       wx.openLocation({
