c9a6ff8a05e7f3de258fec740692f2a2a6836250
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-07-17 14:29:41 +0800

    auto submit

--- exhibit/views.py
+++ exhibit/views.py
@@ -25,7 +25,7 @@ def homeHandler(request):
   shops = Shop.objects.filter(state=2)[:2]
   for shop in shops:
     shop.cover = json.loads(shop.cover)
-    shop.courses = shop.course_set.all()
+    shop.courses = shop.course_set.filter(state=2)
     for i, course in enumerate(shop.courses):
       shop.courses[i].cover = json.loads(course.cover)
   return render(request, 'exhibit/home.html', {'shops' : shops, 'shopSize' : len(shops)})
