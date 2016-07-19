152f48a833f7e75e54c3e30dd55df33930be4ff3
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-07-20 00:00:34 +0800

    auto submit

--- static/exhibit/js/user_verifyPhone.js
+++ static/exhibit/js/user_verifyPhone.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+  submitAction();
+  bindNumberInputCallback(numberInputCallback);
+});
+
+var submitAction = function() {
   $('.submitBtn').click(function() {
     var phone = $('input').val();
     var url = '/user?action=verify&type=phone&phone=' + phone;
@@ -7,4 +12,27 @@ $(document).ready(function() {
       window.location.href = url;
     });
   });
-});
+}
+
+var numberInputCallback = function(value) {
+  // 获取已经输入的所有code
+  var codes = $('.codeBox.active');
+  var allCodes = $('.codeBox');
+  // 如果输入的是回退
+  if (value == 'del') {
+    var val = $('input').val();
+    if (val.length == 0)
+      return false;
+    val = val.substring(0, val.length - 1);
+    $('input').val(val);
+  }
+  // 如果输入的是确认
+  else if (value == 'commit') {
+    $('.submitBtn').trigger('click');
+  } else {
+    // 如果输入的是数字
+    var val = $('input').val();
+    val = val + value;
+    $('input').val(val);
+  }
+}
