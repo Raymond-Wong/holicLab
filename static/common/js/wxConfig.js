304ba7e916ebb35f3b98268b2cea79b1b59b2aec
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-07-25 22:21:09 +0800

    auto submit

--- static/common/js/wxConfig.js
+++ static/common/js/wxConfig.js
@@ -1,11 +1,10 @@
 var wxConfig = function(jsApiList) {
-  FINISHED_LOADING = false;
-  var nonceStr = 'holicLab';
   var appId = 'wx466a0c7c6871bc8e';
-  var timestamp = new Date().getTime();
-  var url = window.location.host + window.location.pathname
-  post('/wechat/config', {'nonceStr' : nonceStr, 'timestamp' : timestamp, 'url' : url}, function(signature) {
-    console.log(signature);
+  var url = window.location.href.split('#')[0];
+  post('/wechat/config', {'url' : url}, function(msg) {
+    var signature = msg['signature'];
+    var timestamp = msg['timestamp'];
+    var nonceStr = msg['noncestr'];
     wx.config({
       debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
       appId: appId, // 必填，公众号的唯一标识
@@ -15,11 +14,27 @@ var wxConfig = function(jsApiList) {
       jsApiList: jsApiList // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
     });
   });
-  wx.ready(function() {
-    FINISHED_LOADING = true;
-  });
-  wx.error(function() {
-    FINISHED_LOADING = true;
+  wx.error(function(res){
+    alert(res);
   });
 }
 
+// $(document).ready(function() {
+//   wxConfig(['onMenuShareAppMessage']);
+//   wx.ready(function() {
+//       wx.onMenuShareAppMessage({
+//       title: '', // 分享标题
+//       desc: '', // 分享描述
+//       link: '', // 分享链接
+//       imgUrl: '', // 分享图标
+//       type: '', // 分享类型,music、video或link，不填默认为link
+//       dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+//       success: function () { 
+//         // 用户确认分享后执行的回调函数
+//       },
+//       cancel: function () { 
+//         // 用户取消分享后执行的回调函数
+//       }
+//     });
+//   });
+// });
