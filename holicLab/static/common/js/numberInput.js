9e1836dfc74ef75a679437dd7c1c1fc2951c2fec
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-07-27 12:11:05 +0800

    auto submit

--- holicLab/static/common/js/numberInput.js
+++ holicLab/static/common/js/numberInput.js
@@ -1,6 +1,5 @@
 var bindNumberInputCallback = function(callback) {
   $('.numberInputBtn').on('tap', function() {
-    $('body').prepend('tap1 ');
     var value = $(this).attr('value');
     callback(value);
     return false;
