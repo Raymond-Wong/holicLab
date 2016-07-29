98e862e73da606d572efde0b460d937277c05e4a
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-07-29 19:09:34 +0800

    auto submit

--- exhibit/views.py
+++ exhibit/views.py
@@ -17,7 +17,7 @@ def testHandler(request):
   user = {}
   user['gender'] = 'm'
   user['role'] = '1'
-  return render(request, 'exhibit/order_list.html')
+  return render(request, 'exhibit/order_get.html')
 
 # 微信端入口
 @handler
