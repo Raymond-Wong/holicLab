a8862d5a80e16d968828d7222966e9c75ebb4564
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-08-04 22:16:43 +0800

    auto submit

--- exhibit/handlers/notify.py
+++ exhibit/handlers/notify.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+import sys
+sys.path.append('..')
+reload(sys)
+sys.setdefaultencoding('utf-8')
+import json
+from datetime import timedelta
+try: 
+  import xml.etree.cElementTree as ET
+except ImportError: 
+  import xml.etree.ElementTree as ET
+
+from django.http import HttpResponse, HttpRequest, HttpResponseServerError, Http404
+from django.utils.encoding import smart_str
+
+from holicLab.utils import *
+from holicLab.models import Order, Shop, User, Course, Bookable_Time
+
 def notify(request):
   # 待返回信息
   RET_STR = '<xml><return_code><![CDATA[%s]]></return_code><return_msg><![CDATA[%s]]></return_msg></xml>'
