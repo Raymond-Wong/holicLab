e0ba9517594208c09ece53d2637f1cf275e96d6c
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-08-06 23:06:32 +0800

    auto submit

--- static/exhibit/js/home.js
+++ static/exhibit/js/home.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   wxConfig(['onMenuShareAppMessage']);
   wx.ready(function() {
-    alert(wx.invoke);
+    alert(wx.trigger);
   });
   myScroll = new IScroll('#courseWrapper', { scrollX: true, scrollY: false, mouseWheel: true, eventPassthrough: true});
 })
