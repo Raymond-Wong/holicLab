c66fe137f4570cf9091317345e40533f2cee1e6f
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-08-06 23:38:48 +0800

    auto submit

--- static/exhibit/js/user_invite_record.js
+++ static/exhibit/js/user_invite_record.js
@@ -60,10 +60,13 @@ var initShare = function() {
     wx.showMenuItems({
       menuList: ['menuItem:share:appMessage'] // 要显示的菜单项，所有menu项见附录3
     });
+    var link = window.location.host + '/user?action=invite&invited=' + uid;
+    link = encodeURIComponent(link);
+    link = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx8a6f32cf9d22a289&redirect_uri=' + link + '&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
     wx.onMenuShareAppMessage({
       title: '来一次HolicLab吧，优惠拿去别客气！', // 分享标题
       desc: '首次预约立享五折，无需年卡，永不打烊，24小时不停摆只等你来练🏋', // 分享描述
-      link: window.location.host + '/user?action=invite&invited=' + uid, // 分享链接
+      link: link, // 分享链接
       imgUrl: 'http://' + window.location.host + '/static/exhibit/res/user_invited.jpg', // 分享图标
       type: 'link', // 分享类型,music、video或link，不填默认为link
       dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
