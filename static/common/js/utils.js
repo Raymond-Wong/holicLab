c0788da36d9a3c5ab997519f668cf66fc2e3b67d
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-08-06 23:14:10 +0800

    auto submit

--- static/common/js/utils.js
+++ static/common/js/utils.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
-  wxConfig(['hideOptionMenu', 'closeWindow']);
+  wxConfig(['hideOptionMenu', 'showMenuItems']);
   wx.ready(function() {
     wx.hideOptionMenu();
-    wx.closeWindow();
+    wx.showMenuItems({
+      menuList: ['menuItem:share:appMessage'] // 要显示的菜单项，所有menu项见附录3
+    });
   });
 });
 
