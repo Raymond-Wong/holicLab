576b131c779c09d88e3e8f3fe08d9edeae356f09
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-08-11 23:51:52 +0800

    auto submit

--- holicLab/utils.py
+++ holicLab/utils.py
@@ -90,9 +90,8 @@ def send_xml(url, data):
   return urllib2.urlopen(url, data, timeout=30).read()
 
 def send_xml_ssl(url, data):
-  print sys.path
-  certPath = '/data1/www/htdocs/898/holicLab/1/static/wechat/cert/apiclient_cert.pem'
-  keyPath = '/data1/www/htdocs/898/holicLab/1/static/wechat/cert/apiclient_key.pem'
+  certPath = '%s/static/wechat/cert/apiclient_cert.pem' % sys.path[0]
+  keyPath = '%s/static/wechat/cert/apiclient_key.pem' % sys.path[0]
   return requests.post(url, cert=(certPath, keyPath), data=data).text
 
 CODE_RANGE = [str(i) for i in xrange(0, 10)] + [chr(i) for i in xrange(97, 123)] + [chr(i) for i in xrange(65, 91)]
