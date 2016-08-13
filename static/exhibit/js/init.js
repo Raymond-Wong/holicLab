a01cc8452509ec25a53efd70f7fd1fe7be4f669a
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-08-13 23:56:39 +0800

    auto submit

--- static/exhibit/js/init.js
+++ static/exhibit/js/init.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   wx.ready(function() {
     var list = ['menuItem:share:timeline', 'menuItem:share:qq', 'menuItem:share:weiboApp', 'menuItem:favorite', 'menuItem:share:facebook', 'menuItem:share:QZone',
                 'menuItem:editTag', 'menuItem:delete', 'menuItem:copyUrl', 'menuItem:originPage', 'menuItem:readMode', 'menuItem:openWithQQBrowser', 'menuItem:openWithSafari', 'menuItem:share:email', 'menuItem:share:brand'];
+    alert(window.location.href);
     if (window.location.href.indexOf('/order?action=get') < 0 &&
         window.location.href.indexOf('/user?action=invite') < 0) {
       list.push('menuItem:share:appMessage');
