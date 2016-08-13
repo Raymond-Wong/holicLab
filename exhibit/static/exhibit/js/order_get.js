4e700da2baf5b9cff328d02c9a66f700e8a9b8ad
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-08-14 00:01:28 +0800

    auto submit

--- exhibit/static/exhibit/js/order_get.js
+++ exhibit/static/exhibit/js/order_get.js
@@ -41,11 +41,11 @@ var initInviteAction = function() {
 }
 
 var initShare = function() {
-  wxConfig(['onMenuShareAppMessage',]);
+  wxConfig(['onMenuShareAppMessage', 'showMenuItems']);
   wx.ready(function() {
-    // wx.showMenuItems({
-    //   menuList: ['menuItem:share:appMessage'] // 要显示的菜单项，所有menu项见附录3
-    // });
+    wx.showMenuItems({
+      menuList: ['menuItem:share:appMessage'] // 要显示的菜单项，所有menu项见附录3
+    });
     var link = 'http://' + window.location.host + '/user?action=invite&invited=' + uid;
     link = encodeURIComponent(link);
     link = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx8a6f32cf9d22a289&redirect_uri=' + link + '&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
