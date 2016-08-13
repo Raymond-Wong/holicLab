d0367b3f5413a254976e1e004cc566419da9f5c6
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-08-13 22:47:42 +0800

    auto submit

--- static/exhibit/js/init.js
+++ static/exhibit/js/init.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  // wxConfig(['hideOptionMenu']);
-  // wx.ready(function() {
-  //   wx.hideOptionMenu();
-  // });
+  wxConfig(['hideAllNonBaseMenuItem']);
+  wx.ready(function() {
+    wx.hideAllNonBaseMenuItem();
+  });
 });
