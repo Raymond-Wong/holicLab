5ffac912c56ffd2cc9672efdf467091102a7b6f9
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-08-13 23:57:21 +0800

    auto submit

--- static/exhibit/js/init.js
+++ static/exhibit/js/init.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
   wx.ready(function() {
     var list = ['menuItem:share:timeline', 'menuItem:share:qq', 'menuItem:share:weiboApp', 'menuItem:favorite', 'menuItem:share:facebook', 'menuItem:share:QZone',
                 'menuItem:editTag', 'menuItem:delete', 'menuItem:copyUrl', 'menuItem:originPage', 'menuItem:readMode', 'menuItem:openWithQQBrowser', 'menuItem:openWithSafari', 'menuItem:share:email', 'menuItem:share:brand'];
-    alert(window.location.href);
+    alert(window.location.href.indexOf('/order?action=get'));
+    alert(window.location.href.indexOf('/user?action=invite'));
     if (window.location.href.indexOf('/order?action=get') < 0 &&
         window.location.href.indexOf('/user?action=invite') < 0) {
       list.push('menuItem:share:appMessage');
