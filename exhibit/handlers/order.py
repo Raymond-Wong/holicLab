df518206cc22e47df467b463018bfc0bb777a117
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-08-14 22:52:55 +0800

    auto submit

--- exhibit/handlers/order.py
+++ exhibit/handlers/order.py
@@ -152,7 +152,6 @@ def refund(request):
   res = send_xml_ssl('https://api.mch.weixin.qq.com/secapi/pay/refund', msg)
   res = ET.fromstring(smart_str(res))
   res = xml2dict(res)
-  print res
   if res.has_key('return_code') and res['return_code'] == 'SUCCESS' and res.has_key('result_code') and res['result_code'] == 'SUCCESS':
     cancelSuccess(order)
     return HttpResponse(Response(m=float(res['refund_fee']) / 10 / 10).toJson(), content_type="application/json")
