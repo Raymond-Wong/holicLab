de9f9e84993b3ded5f938835ea8c0a48889bc72a
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-08-14 22:01:35 +0800

    auto submit

--- exhibit/handlers/notify.py
+++ exhibit/handlers/notify.py
@@ -16,7 +16,7 @@ from django.db.models import F
 
 from holicLab.utils import *
 from holicLab.models import Order, Shop, User, Course, Bookable_Time, Time_Bucket
-from pay import getOrderPrice
+from pay import getOrderPrice, successOrder
 
 def notify(request):
   # 待返回信息
@@ -40,50 +40,3 @@ def notify(request):
 # 验证签名是否正确
 def verifySign(params, order):
   return True
-
-# 订单支付成功后修改数据
-def successOrder(order, status, time_end):
-  # 1. 修改订单的状态
-  if status == 'SUCCESS':
-    order.state = "4"
-    order.pay_time = datetime.strptime(time_end, '%Y%m%d%H%M%S')
-  else:
-    order.delete()
-    return None
-  user = order.user
-  # 2. 如果该用户是新用户且是被邀请用户的话，邀请他的用户可以获得优惠券
-  if user.invited_by and user.user_type == "1":
-    invite_user = user.invited_by
-    invite_user.balance = F('balance') + 1
-    invite_user.save()
-  # 3. 设置该用户为老用户
-  user.user_type = "2"
-  # 2.1 更新该用户的优惠券数量
-  price, usedCoupon = getOrderPrice(order, (order.end_time - order.start_time).seconds / 60)
-  user.balance = F('balance') - usedCoupon
-  user.balance = F('balance') if F('balance') >= 0 else 0
-  # 4. 修改订单涉及课程或者场地的占用人次
-  if order.order_type == "1":
-    shop = order.shop
-    duration = int((order.end_time - order.start_time).total_seconds()) / 60
-    for period in xrange(duration / 30):
-      start_time = order.start_time + timedelta(seconds=60*30*period)
-      timeBucket = None
-      try:
-        timeBucket = Time_Bucket.objects.filter(shop=shop).get(start_time=start_time)
-      except:
-        timeBucket = Time_Bucket()
-        timeBucket.start_time = start_time
-        timeBucket.shop = order.shop
-        timeBucket.occupation = 0
-        timeBucket.save()
-      timeBucket.occupation = F('occupation') + 1
-      timeBucket.save()
-  else:
-    course = order.course
-    bookableTime = Bookable_Time.objects.filter(course=course).get(start_time=order.start_time)
-    bookableTime.occupation = F('occupation') + 1
-    bookableTime.save()
-  # 保存对象
-  order.save()
-  user.save()
--- exhibit/handlers/pay.py
+++ exhibit/handlers/pay.py
@@ -25,7 +25,6 @@ from django.db.models import F
 import holicLab.settings as settings
 from holicLab.utils import *
 from holicLab.models import Order, Shop, User, Course, Bookable_Time, Time_Bucket
-from notify import successOrder
 
 def add(request):
   user = User.objects.get(invite_code=request.session['user'])
@@ -288,3 +287,50 @@ def getPrePayId(order, request):
   if res['return_code'] == 'SUCCESS' and res['return_msg'] == 'OK':
     return True, res['prepay_id']
   return False, '发起支付请求失败，请联系客服人员'
+
+# 订单支付成功后修改数据
+def successOrder(order, status, time_end):
+  # 1. 修改订单的状态
+  if status == 'SUCCESS':
+    order.state = "4"
+    order.pay_time = datetime.strptime(time_end, '%Y%m%d%H%M%S')
+  else:
+    order.delete()
+    return None
+  user = order.user
+  # 2. 如果该用户是新用户且是被邀请用户的话，邀请他的用户可以获得优惠券
+  if user.invited_by and user.user_type == "1":
+    invite_user = user.invited_by
+    invite_user.balance = F('balance') + 1
+    invite_user.save()
+  # 3. 设置该用户为老用户
+  user.user_type = "2"
+  # 2.1 更新该用户的优惠券数量
+  price, usedCoupon = getOrderPrice(order, (order.end_time - order.start_time).seconds / 60)
+  user.balance = F('balance') - usedCoupon
+  user.balance = F('balance') if F('balance') >= 0 else 0
+  # 4. 修改订单涉及课程或者场地的占用人次
+  if order.order_type == "1":
+    shop = order.shop
+    duration = int((order.end_time - order.start_time).total_seconds()) / 60
+    for period in xrange(duration / 30):
+      start_time = order.start_time + timedelta(seconds=60*30*period)
+      timeBucket = None
+      try:
+        timeBucket = Time_Bucket.objects.filter(shop=shop).get(start_time=start_time)
+      except:
+        timeBucket = Time_Bucket()
+        timeBucket.start_time = start_time
+        timeBucket.shop = order.shop
+        timeBucket.occupation = 0
+        timeBucket.save()
+      timeBucket.occupation = F('occupation') + 1
+      timeBucket.save()
+  else:
+    course = order.course
+    bookableTime = Bookable_Time.objects.filter(course=course).get(start_time=order.start_time)
+    bookableTime.occupation = F('occupation') + 1
+    bookableTime.save()
+  # 保存对象
+  order.save()
+  user.save()
