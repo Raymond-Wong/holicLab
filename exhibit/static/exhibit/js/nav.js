153d2b33a0d4b3edb84f18abb08a9df1752bf4e6
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-08-15 12:32:46 +0800

    auto submit

--- exhibit/static/exhibit/js/nav.js
+++ exhibit/static/exhibit/js/nav.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
   $('#getPwdBtn').on('tap', function() {
     showToast('正在获取密码...');
     post('/order?action=password', {}, function(msg) {
-      hideToast()
       window.location.href = msg;
-    })
+    });
+    hideToast();
   });
 });
