db1c8425a1bd466f590f4ab6908b9c256e24a3f8
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-08-20 22:15:19 +0800

    auto submit

--- exhibit/static/exhibit/js/load.js
+++ exhibit/static/exhibit/js/load.js
@@ -3,13 +3,12 @@ $(document).ready(function() {
 });
 
 var loading = function() {
-  alert(FINISHED_LOADING);
   try {
     // 如果存在这个变量，那么当变量为真时结束加载
     if (FINISHED_LOADING) {
       finishLoading();
     } else {
-      setTimeout(loading, 1000);
+      setTimeout(loading, 2000);
     }
   } catch (e) {
     // 如果不存在这个变量，则直接结束加载
