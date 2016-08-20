af6aff65626dbc83d67e24d16e69f4d24a641705
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-08-20 14:11:17 +0800

    auto submit

--- exhibit/static/exhibit/js/user_tags.js
+++ exhibit/static/exhibit/js/user_tags.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
   $('#submitBtn').click(function() {
     var gender = $('.radio.checked[name="gender"]').attr('value');
     var role = $('.radio.checked[name="role"]').attr('value');
+    showToast('数据正在录入...');
     post('/user?action=update', {'gender' : gender, 'role' : role}, function(msg) {
-      alert('数据录入成功，即将自动跳转');
+      showToast("前往主页中...");
       setTimeout(function() {
         window.location.href = msg;
       }, 1500);
