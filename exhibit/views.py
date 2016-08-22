3ab99ae82713638f087405a9f118c11df27bbb8e
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-08-22 16:26:08 +0800

    auto submit

--- exhibit/views.py
+++ exhibit/views.py
@@ -15,7 +15,10 @@ from holicLab.models import *
 import handlers.shop, handlers.course, handlers.user, handlers.login, handlers.order, handlers.pay, handlers.notify
 
 def testHandler(request):
-  return render(request, 'exhibit/order_success.html')
+  shop = {}
+  shop['location'] = 'asdfasdfasdf'
+  shop['price'] = 100
+  return render(request, 'exhibit/shop_detail.html')
 
 # 错误提示类
 def errorHandler(request):
