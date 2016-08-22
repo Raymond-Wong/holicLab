7364c3ca65355f70aef4a39e4c034a7589a9584d
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-08-22 21:31:27 +0800

    auto submit

--- exhibit/static/exhibit/js/alertBox.js
+++ exhibit/static/exhibit/js/alertBox.js
@@ -1,4 +1,5 @@
 var mobiAlert = function(msg, callback) {
+  alert('msg');
   var alertBoxContainer = $('#alertBoxContainer');
   var alertBoxText = $(alertBoxContainer.find('.alertText')[0]);
   var alertBtn = $(alertBoxContainer.find('.alertBtn')[0]);
