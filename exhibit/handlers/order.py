f050f5e0c7d3678ca5a8412c6dae0c4ef6c26f46
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-08-23 17:31:20 +0800

    auto submit

--- exhibit/handlers/order.py
+++ exhibit/handlers/order.py
@@ -212,7 +212,7 @@ def success(request):
   return render(request, 'exhibit/order_success.html', {'user' : user})
 
 def isOrderCancelable(order, user):
-  if order.state == "2" or (order.start_time + timedelta(seconds=60 * 15)) <= timezone.now():
+  if order.state == "2" or (order.start_time - timedelta(seconds=60 * 15)) <= timezone.now():
     return False
   elif not belongTo(order, user):
     return False
