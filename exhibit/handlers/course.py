f92366c5c7486678227202db80f0b4abb4600455
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-08-26 00:45:31 +0800

    auto submit

--- exhibit/handlers/course.py
+++ exhibit/handlers/course.py
@@ -17,26 +17,29 @@ from holicLab.models import Shop, Time_Bucket, Course, Service
 # 列出所有课程
 def list(request):
   sid = request.GET.get('sid', None)
-  shop = None
+  shops = []
   # 判断sid合法性
   if sid is None:
-    return HttpResponse(Response(c=-9, m="未提供sid").toJson(), content_type="application/json")
-  try:
-    shop = Shop.objects.get(id=int(sid))
-  except Exception, e:
-    return HttpResponse(Response(c=-3, m="待查询商店不存在").toJson(), content_type="application/json")
-  allCourses = shop.course_set.filter(state=2).order_by('-releasedDate')
+    # return HttpResponse(Response(c=-9, m="未提供sid").toJson(), content_type="application/json")
+    shops = Shop.objects.all()
+  else:
+    try:
+      shops.append(Shop.objects.get(id=int(sid)))
+    except Exception, e:
+      return HttpResponse(Response(c=-3, m="待查询商店不存在").toJson(), content_type="application/json")
   courses = []
-  # 将图片列表从字符串处理成数组
-  now = timezone.now()
-  for course in allCourses:
-    course.cover = json.loads(course.cover)
-    course.price = course.price / 10 if course.price % 10 == 0 else course.price / 10.0
-    course.bookable_time = course.bookable_time_set.order_by('-start_time')
-    if len(course.bookable_time) > 0:
-      course.start_time = course.bookable_time[0].start_time
-      course.end_time = course.bookable_time[0].end_time
-      courses.append(course)
+  for shop in shops:
+    allCourses = shop.course_set.filter(state=2).order_by('-releasedDate')
+    # 将图片列表从字符串处理成数组
+    now = timezone.now()
+    for course in allCourses:
+      course.cover = json.loads(course.cover)
+      course.price = course.price / 10 if course.price % 10 == 0 else course.price / 10.0
+      course.bookable_time = course.bookable_time_set.order_by('-start_time')
+      if len(course.bookable_time) > 0:
+        course.start_time = course.bookable_time[0].start_time
+        course.end_time = course.bookable_time[0].end_time
+        courses.append(course)
   return render(request, 'exhibit/course_list.html', {'courses' : courses})
 
 # 显示商店详情
