464edafa9df032c423fe023533343031c314b407
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-09-21 23:53:34 +0800

    auto submit

--- exhibit/static/exhibit/js/alertBox.js
+++ exhibit/static/exhibit/js/alertBox.js
@@ -8,7 +8,8 @@ var mobiAlert = function(msg, callback) {
   confirmBtnBox.hide();
   alertBtnBox.show();
   alertBoxContainer.show();
-  alertBtn.on('tap', function(evt) {
+  alertBtn.unbind('tap');
+  alertBtn.bind('tap', function(evt) {
     evt.preventDefault();
     alertBoxContainer.hide();
     alertBoxText.html('');
