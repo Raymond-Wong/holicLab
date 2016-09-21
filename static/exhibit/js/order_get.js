77442e045b33c38c1d599b1f62ead925eca6e58e
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-09-21 23:52:41 +0800

    auto submit

--- static/exhibit/js/order_get.js
+++ static/exhibit/js/order_get.js
@@ -24,6 +24,7 @@ var initCancelAction = function() {
           }, 1500);
         } else {
           mobiAlert(msg, function() {
+            alert('asfd');
             hideToast();
           });
         }
