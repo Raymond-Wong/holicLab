b65a8e918d768a37e736bde563b3af354da0b8ac
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-09-25 20:18:18 +0800

    auto submit

--- exhibit/static/exhibit/js/myLink.js
+++ exhibit/static/exhibit/js/myLink.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
   $('.myLink, a').on('tap', function() {
-    if ($(this).hasClass('notMyLink')) {
-      return true;
-    }
     var link = $(this).attr('href');
     window.location.href = link;
     return false;
