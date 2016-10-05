9c5bd768df02de9b8055fa40372eae3445f15045
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-10-05 22:08:11 +0800

    auto submit

--- exhibit/handlers/pay.py
+++ exhibit/handlers/pay.py
@@ -331,7 +331,7 @@ def successOrder(order, status, time_end):
   tmpPrice, usedCoupon = getCouponPrice(order.price, user.balance, (order.end_time - order.start_time).seconds / 60)
   user.balance = F('balance') - usedCoupon
   user.balance = F('balance') if F('balance') >= 0 else 0
-  print 'user.balance=%d, usedCoupon=%d' % (user.balance, usedCoupon)
+  print 'usedCoupon=%d' % usedCoupon
   # 2.2 更新用户消费总金额
   user.consumption = F('consumption') + order.price
   # 4. 修改订单涉及课程或者场地的占用人次
