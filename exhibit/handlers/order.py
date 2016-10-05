f6dddb88b2d209221c7e09a1273613e2e3159003
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-10-05 22:44:35 +0800

    auto submit

--- exhibit/handlers/order.py
+++ exhibit/handlers/order.py
@@ -202,7 +202,6 @@ def cancelSuccess(order):
   order.state = "2"
    # 2. 更新用户的总消费金额以及优惠券数量
   user.consumption = F('consumption') - order.price
-  print 'usedCoupon=%d' % usedCoupon
   # 如果该用户除了当前订单没有其他订单
   if len(user.order_set.filter(state="4")) == 0 and user.invited_by:
     # 3. 减少邀请该用户的用户的抵扣券
